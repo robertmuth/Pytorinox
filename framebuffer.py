2d4dbf28b80adf95fc18ce4ca55da0bc71f471f1
Robert Muth <robert@muth.org>
2019-08-04 10:56:25 -0400

    framebuffer improvements

--- framebuffer.py
+++ framebuffer.py
@@ -11,6 +11,7 @@ bits_per_pixel    assumed memory layout
 """
 
 from PIL import Image
+import numpy
 
 
 def _read_and_convert_to_ints(filename):
@@ -26,17 +27,47 @@ def _converter_argb(image: Image):
 
 def _converter_rgb565(image: Image):
     return bytes([x for r, g, b in image.getdata()
-                  for x in ((g & 0x1c) << 3 | (b >> 3),  r & 0xf8 | (g >> 5))])
+                  for x in ((g & 0x1c) << 3 | (b >> 3), r & 0xf8 | (g >> 3))])
 
 
-def _converter_rgb(image: Image):
+def _converter_1_argb(image: Image):
+    return bytes([x for p in image.getdata()
+                  for x in (255, p, p, p)])
+
+
+def _converter_1_rgb(image: Image):
+    return bytes([x for p in image.getdata()
+                  for x in (p, p, p)])
+
+
+def _converter_1_rgb565(image: Image):
+    return bytes([(255 if x else 0) for p in image.getdata()
+                  for x in (p, p)])
+
+
+def _converter_rgba_rgb565_numpy(image: Image):
+    flat = numpy.frombuffer(image.tobytes(), dtype=numpy.uint32)
+    # note,  this is assumes little endian byteorder and results in
+    # the following packing of an integer:
+    # bits 0-7: red, 8-15: green, 16-23: blue, 24-31: alpha
+    flat = ((flat & 0xf8) << 8) | ((flat & 0xfc00) >> 5) | ((flat & 0xf80000) >> 19)
+    return flat.astype(numpy.uint16).tobytes()
+
+
+def _converter_no_change(image: Image):
     return image.tobytes()
 
-
+# anything that does not use numpy is hopelessly slow
 _CONVERTER = {
-    16: _converter_rgb565,
-    24: _converter_rgb,
-    32: _converter_argb,
+    ("RGBA", 16): _converter_rgba_rgb565_numpy,
+    ("RGB", 16): _converter_rgb565,
+    ("RGB", 24): _converter_no_change,
+    ("RGB", 32): _converter_argb,
+    # note numpy does not work well with mode="1" images as
+    # image.tobytes() loses pixel color info
+    ("1", 16): _converter_1_rgb565,
+    ("1", 24): _converter_1_rgb,
+    ("1", 32): _converter_1_argb,
 }
 
 
@@ -50,8 +81,6 @@ class Framebuffer(object):
         self.stride = _read_and_convert_to_ints(config_dir + "/stride")[0]
         self.bits_per_pixel = _read_and_convert_to_ints(
             config_dir + "/bits_per_pixel")[0]
-        self._converter = _CONVERTER.get(self.bits_per_pixel)
-        assert self._converter is not None
         assert self.stride == self.bits_per_pixel // 8 * self.size[0]
 
     def __str__(self):
@@ -60,20 +89,27 @@ class Framebuffer(object):
 
     # Note: performance is terrible even for medium resolutions
     def show(self, image: Image):
-        assert image.mode == "RGB"
+        converter = _CONVERTER[(image.mode, self.bits_per_pixel)]
         assert image.size == self.size
-        out = self._converter(image)
+        out = converter(image)
         with open(self.path, "wb") as fp:
             fp.write(out)
 
+    def on(self):
+        pass
+
+    def off(self):
+        pass
 
 if __name__ == "__main__":
     import time
     from PIL import ImageDraw
-    for i in range(2):
+
+
+    def TestFrameBuffer(i):
         fb = Framebuffer(i)
-        print (fb)
-        image = Image.new("RGB", fb.size)
+        print(fb)
+        image = Image.new("RGBA", fb.size)
         draw = ImageDraw.Draw(image)
         draw.rectangle(((0, 0), fb.size), fill="green")
         draw.ellipse(((0, 0), fb.size), fill="blue", outline="red")
@@ -82,4 +118,8 @@ if __name__ == "__main__":
         for i in range(5):
             fb.show(image)
         stop = time.time()
-        print ("fps: %.2f" % (10 / (stop - start)))
+        print("fps: %.2f" % (10 / (stop - start)))
+
+
+    for i in [1]:
+        TestFrameBuffer(i)
